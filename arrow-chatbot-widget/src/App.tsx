0baab050c33fc18e89d87fbfad1d02f80f3b64a1
ayebaishmo <ishmo255super@gmail.com>
2026-01-22 13:21:47 +0300

    Enviroment and app clean up

---
+++ arrow-chatbot-widget/src/App.tsx
@@ -0,0 +1,19 @@
+
+function App() {
+  return (
+    <>
+      <div>
+        
+      </div>
+      <h1>Welcome to Arrow chatbot</h1>
+      <div className="card">
+        
+      </div>
+      <p className="read-the-docs">
+        More is coming
+      </p>
+    </>
+  )
+}
+
+export default App
