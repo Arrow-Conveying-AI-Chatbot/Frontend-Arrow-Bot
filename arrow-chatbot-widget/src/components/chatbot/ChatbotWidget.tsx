70694c84823997990e6a5c823ab1962ace3214fe
ayebaishmo <ishmo255super@gmail.com>
2026-01-23 11:42:28 +0300

    Chatbot Widget Layout

---
+++ arrow-chatbot-widget/src/components/chatbot/ChatbotWidget.tsx
@@ -0,0 +1,22 @@
+import "./ChatbotWidget.css";
+
+const ChatbotWidget = () => {
+  return (
+    <div className="chatbot-container">
+      {/* Header */}
+      <div className="chatbot-header">
+        <span>Chat Support</span>
+      </div>
+
+      {/* Messages Area */}
+      <div className="chatbot-messages">
+      </div>
+
+      {/* Input Area */}
+      <div className="chatbot-input">
+      </div>
+    </div>
+  );
+};
+
+export default ChatbotWidget;
